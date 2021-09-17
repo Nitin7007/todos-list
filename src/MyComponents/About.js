687031776b783b7c3b53cc30c2e22c2def2c70ad
Nitin Aneja <nitinaneja00@gmail.com>
2021-09-17 23:09:09 +0530

    Initial Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        eaque praesentium quia error cupiditate, dolore aliquid molestias.
+        Incidunt velit fuga autem voluptatum aperiam exercitationem odit numquam
+        libero excepturi. Quibusdam iste accusantium voluptatem similique error!
+      </p>
+    </div>
+  );
+};
